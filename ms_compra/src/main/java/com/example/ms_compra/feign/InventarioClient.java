f82e9decf4ea0bd96060bd2fd4fa722b176a95fe
Juan Anderson <juancoasacahuanca@gmail.com>
2025-06-04 23:30:04 -0500

    Tu mensaje de commit describiendo los cambios

--- ms_compra/src/main/java/com/example/ms_compra/feign/InventarioClient.java
+++ ms_compra/src/main/java/com/example/ms_compra/feign/InventarioClient.java
@@ -1,16 +1,19 @@
 package com.example.ms_compra.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "ms-inventario-service", url = "${feign.inventario.url}")
 public interface InventarioClient {
-    @PostMapping("/api/stock/{productoId}/reponer")
+    @PostMapping("/{productoId}/reponer")
     void reponeStock(@RequestParam Long productoId,
                      @RequestParam Integer cantidad);
 
-    @PostMapping("/api/stock/{productoId}/reservar")
+    @PostMapping("/{productoId}/reservar")
     void reservaStock(@RequestParam Long productoId,
                       @RequestParam Integer cantidad);
+
 }
