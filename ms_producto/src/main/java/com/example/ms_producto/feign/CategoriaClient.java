b39658cecc2b0b0d410cd254cf2516d5c8fb0a50
Juan Anderson <juancoasacahuanca@gmail.com>
2025-06-11 08:14:33 -0500

    Tu mensaje de commit describiendo los cambios

--- ms_producto/src/main/java/com/example/ms_producto/feign/CategoriaClient.java
+++ ms_producto/src/main/java/com/example/ms_producto/feign/CategoriaClient.java
@@ -5,9 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-categoria", path = "/api/categorias")
+@FeignClient(name = "ms-categoria-service", url = "${feign.categoria.url}")
 public interface CategoriaClient {
-
     @GetMapping("/{id}")
     CategoriaDto obtenerPorId(@PathVariable("id") Long id);
 }
